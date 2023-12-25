0ffd2e6a25fad55ffa42c2534766f0a1bf3df3ef
ManasMish <manasmishra845@gmail.com>
2023-12-25 22:25:00 +0530

    adding tabel

---
+++ basicJ/tabel.java
@@ -0,0 +1,16 @@
+package basicJ;
+
+import java.util.Scanner;
+
+public class tabel {
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter th number to print its tabel upto 10 times");
+            int n=sc.nextInt();
+            for(int i=1;i<=10;i++)
+            {
+                System.out.println(n+"*"+i+"="+(n*i));
+            }
+            }
+}
