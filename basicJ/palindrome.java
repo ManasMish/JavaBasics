e6338337c0c0615e0739519eafeb1718a224deac
ManasMish <manasmishra845@gmail.com>
2023-12-25 21:59:10 +0530

    adding palindrome

---
+++ basicJ/palindrome.java
@@ -0,0 +1,28 @@
+package basicJ;
+
+import java.util.Scanner;
+
+public class palindrome {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number to check whether palindrome or not");
+        int n= sc.nextInt();
+        int dup=n;
+        int r;
+        int sum=0;
+        while(n>0)
+        {
+            r=n%10;
+           sum=sum*10+r;
+           n=n/10;
+        }
+        if(sum==dup)
+        {
+            System.out.println("Entered number: " + dup +" is palindrome");
+        }
+        else
+        {
+            System.out.println("Entered number: " + dup +" is not palindrome");
+        }
+    }
+}
