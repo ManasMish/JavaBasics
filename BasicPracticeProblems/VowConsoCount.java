d450291f7ce5b71debe60d00fda4a595bb786412
ManasMish <manasmishra845@gmail.com>
2023-12-27 11:31:05 +0530

    11:30 27 Dec 2023

--- BasicPracticeProblems/VowConsoCount.java
+++ BasicPracticeProblems/VowConsoCount.java
@@ -7,14 +7,23 @@ public class VowConsoCount {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the string to count number of vowels and consonents");
         String c= sc.next();
-        c=c.toLowerCase();
-        int v;
-        int con;
+        c=c.toUpperCase();
+        int v = 0;
+        int con = 0;
         for(int i=0;i<c.length();i++)
         {
             char f=c.charAt(i);
-            System.out.println(f);
+            if((int)f==65||(int)f==69||(int)f==73||(int)f==79||(int)f==85)
+            {
+                v=v+1;
+            }
+            else
+            {
+                con=con+1;
+            }
         }
+        System.out.println("Number of vowels in "+c+" is : "+v);
+        System.out.println("Number of Consonants in "+c+" is : "+con);
         sc.close();
     }
 }
---
+++ BasicPracticeProblems/tempconv.java
@@ -0,0 +1,15 @@
+package BasicPracticeProblems;
+
+import java.util.Scanner;
+
+public class tempconv {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the temperature");
+        String t = sc.next();
+        char u = t.charAt(t.length() - 1);
+        if ((int) u == 70) {
+
+        }
+    }
+}
