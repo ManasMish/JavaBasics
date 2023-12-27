c39ff42f0252e0fb6a718618f9111e7c57a4dd22
ManasMish <manasmishra845@gmail.com>
2023-12-27 12:23:03 +0530

    12:22 27 dec 2023

--- BasicPracticeProblems/tempconv.java
+++ BasicPracticeProblems/tempconv.java
@@ -6,10 +6,21 @@ public class tempconv {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the temperature");
-        String t = sc.next();
-        char u = t.charAt(t.length() - 1);
-        if ((int) u == 70) {
-
+        double t = sc.nextDouble();
+        System.out.println("Enter the unit");
+        String u=sc.next();
+        if((int)u.charAt(0)==70||(int)u.charAt(0)==102)
+        {
+            t=(t-32)*5/9;
+            System.out.println("Temperature in degree celsius is: "+t);
+        } else if ((int)u.charAt(0)==67||(int)u.charAt(0)==99)
+        {
+          t=(t*9/5)+32;
+            System.out.println("Temperature in degree fahrenheit is: "+t);
+        }
+        else
+        {
+            System.out.println("You have entered wrong unit");
         }
     }
 }
